3af0cad6451600031dba446d5816831b68f8089a
ghoul.li <ghoul.li@intelliprogroup.com>
2021-03-02 18:38:15 +0800

    update API port

--- test_api.py
+++ test_api.py
@@ -1,5 +1,5 @@
 import requests
 content = open('test/race_Latino.jpg','rb')
-response= requests.post('http://127.0.0.1:5002/fairface',files={'image': content})
+response= requests.post('http://127.0.0.1:5000/fairface',files={'image': content})
 print(response.status_code)
 print(response.content)
